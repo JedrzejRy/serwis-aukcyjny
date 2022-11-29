4992c6df569f0145280aaa5a7082ec5a508e6cf2
Jedrzej Rynecki <metrexaxa@gmail.com>
2022-11-29 19:46:55 +0100

    Projekt/inicjalizacja

---
+++ src/main/java/com/example/serwisaukcyjny/SerwisAukcyjnyApplication.java
@@ -0,0 +1,13 @@
+package com.example.serwisaukcyjny;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SerwisAukcyjnyApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SerwisAukcyjnyApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/serwisaukcyjny/SerwisAukcyjnyApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.serwisaukcyjny;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SerwisAukcyjnyApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
