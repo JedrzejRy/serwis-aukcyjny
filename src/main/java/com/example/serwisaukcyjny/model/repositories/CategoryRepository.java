00d810b46284e11eb150d78ef3c33365b62967a8
MagdaE <melza@wp.pl>
2022-12-01 20:04:47 +0100

    CategoryRepository

---
+++ src/main/java/com/example/serwisaukcyjny/model/repositories/CategoryRepository.java
@@ -0,0 +1,10 @@
+package com.example.serwisaukcyjny.model.repositories;
+
+import com.example.serwisaukcyjny.model.Category;
+import org.springframework.data.repository.CrudRepository;
+
+public interface CategoryRepository extends CrudRepository <Category, Long> {
+
+
+
+}
