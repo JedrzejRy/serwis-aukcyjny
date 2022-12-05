f1447a54dd40cd318e1e298e676da916643cd5c6
MagdaE <melza@wp.pl>
2022-12-05 20:47:29 +0100

    CommentService

---
+++ src/main/java/com/example/serwisaukcyjny/model/services/CommentService.java
@@ -0,0 +1,21 @@
+package com.example.serwisaukcyjny.model.services;
+
+import com.example.serwisaukcyjny.model.Auction;
+import com.example.serwisaukcyjny.model.Comment;
+import com.example.serwisaukcyjny.model.repositories.CommentRepository;
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Service;
+
+@Service
+@RequiredArgsConstructor
+public class CommentService {
+
+    private final CommentRepository commentRepository;
+
+    public Comment save(Comment comment) {
+        return commentRepository.save(comment);
+    }
+
+
+
+}
