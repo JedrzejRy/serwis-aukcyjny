e06d66dd6af983c4b5614fa41edc0bb746e93fbd
MagdaE <melza@wp.pl>
2022-12-05 20:04:14 +0100

    CategoryRepositoryService.java czyli stworzenie tabeli kategorii

---
+++ src/main/java/com/example/serwisaukcyjny/model/services/CategoryRepositoryService.java
@@ -0,0 +1,34 @@
+package com.example.serwisaukcyjny.model.services;
+
+import com.example.serwisaukcyjny.model.Category;
+import com.example.serwisaukcyjny.model.repositories.CategoryRepository;
+import lombok.RequiredArgsConstructor;
+import org.springframework.boot.CommandLineRunner;
+import org.springframework.stereotype.Service;
+
+@Service
+@RequiredArgsConstructor
+public class CategoryRepositoryService implements CommandLineRunner {
+
+    private final CategoryRepository categoryRepository;
+
+    @Override
+    public void run(String... args) throws Exception {
+        categoryRepository.save(new Category("Moda"));
+        categoryRepository.save(new Category("Elektronika"));
+        categoryRepository.save(new Category("Dom i Ogród"));
+        categoryRepository.save(new Category("Supermarket"));
+        categoryRepository.save(new Category("Dziecko"));
+        categoryRepository.save(new Category("Uroda"));
+        categoryRepository.save(new Category("Zdrowie"));
+        categoryRepository.save(new Category("Kultura i rozrywka"));
+        categoryRepository.save(new Category("Sport i turystyka"));
+        categoryRepository.save(new Category("Sport i turystyka"));
+        categoryRepository.save(new Category("Motoryzacja"));
+        categoryRepository.save(new Category("Nieruchomości"));
+        categoryRepository.save(new Category("Kolekcje i sztuka"));
+        categoryRepository.save(new Category("Firma i usługi"));
+
+
+    }
+}
