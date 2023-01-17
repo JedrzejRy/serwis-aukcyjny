280f4aad3540dee73a98344208411fa614ea2841
Jedrzej Rynecki <metrexaxa@gmail.com>
2023-01-17 13:09:38 +0100

    FIX/ only open auction visible on auction list page

--- src/main/java/com/example/serwisaukcyjny/form/CreateLocalizationForm.java
+++ src/main/java/com/example/serwisaukcyjny/form/CreateLocalizationForm.java
@@ -2,6 +2,7 @@ package com.example.serwisaukcyjny.form;
 
 
 import com.example.serwisaukcyjny.model.Localization;
+import jakarta.validation.constraints.NotBlank;
 import lombok.Getter;
 import lombok.Setter;
 import lombok.ToString;
@@ -11,7 +12,9 @@ import lombok.ToString;
 @ToString
 public class CreateLocalizationForm {
 
+    @NotBlank(message = "wprowadz nazwę ulicy")
     private String address;
+    @NotBlank(message = "Wprowadz nazwę miasta")
     private String city;
     private Localization.Voivodeship voivodeship;
 
